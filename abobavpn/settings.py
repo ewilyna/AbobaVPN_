251d1a922262b095224aa65343d73d56554cd12b
ewilyna <161340295+ewilyna@users.noreply.github.com>
2025-05-28 22:48:31 +0300

    add file 3

--- abobavpn/settings.py
+++ abobavpn/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Для разработки (если ещё не было)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',  # Папка 'static' в корне проекта
